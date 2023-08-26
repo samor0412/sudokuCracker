822b11d9a8eb8fb414da6531d28418417f09d1b3
Ka Long Or <samor412@Kas-MacBook-Air.local>
2023-08-26 19:43:10 +0800

    refactor: cleanup

--- test/level.test.ts
+++ test/level.test.ts
@@ -5,16 +5,16 @@ import { Sudoku } from '../Sudoku';
 const createTestCase = (level: string) => test(level, () => {
     const sudoku = Sudoku.getSudokuFromFile(`./test/questions/${level}.txt`)
     const sudokuCracker = new SudokuCracker(sudoku)
-    console.log(`question:\n${sudoku.print()}`)
+    // console.log(`question:\n${sudoku.print()}`)
     sudokuCracker.execute();
 
     const answerSodoku = Sudoku.getSudokuFromFile(`./test/answers/${level}.txt`)
-    console.log(`resolved answer:\n${answerSodoku.print()}`)
+    // console.log(`resolved answer:\n${answerSodoku.print()}`)
     expect(answerSodoku.print()).toBe(sudoku.print())
 })
 
 describe('SudokuCracker', () => {
     createTestCase('level1')
     createTestCase('level2')
-    // createTestCase('level3')
+    createTestCase('level3')
 })
